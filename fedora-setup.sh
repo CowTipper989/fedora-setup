00ec0cb3e7d44629b4fb9182e238ebc0325cc276
Luke Wells <luke@lukewells.dev>
2023-10-19 11:49:45 -0500

    Added github desktop setup script

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -21,7 +21,7 @@ OPTIONS=(1 "Enable RPM Fusion - Enables the RPM Fusion repos for your specific v
          4 "Enable Flatpak - Enables the Flatpak repo and installs packages located in flatpak-packages.txt"
          5 "Install Software - Installs software located in dnf-packages.txt"
          6 "Install Oh-My-ZSH - Installs Oh-My-ZSH along with Starship prompt"
-         7 "Install Extras - Themes Fonts and Codecs"
+         7 "Install Github Desktop"
          8 "Install Nvidia - Install akmod Nvidia drivers"
          9 "Install Tailscale & VSCode"
          10 "Configure Git"
@@ -68,6 +68,11 @@ while [ "$CHOICE -ne 4" ]; do
            ;;
         5)  echo "Installing Software"
             sudo dnf install -y $(cat dnf-packages.txt)
+            # Github Desktop
+            sudo rpm --import https://rpm.packages.shiftkey.dev/gpg.key
+            sudo sh -c 'echo -e "[shiftkey-packages]\nname=GitHub Desktop\nbaseurl=https://rpm.packages.shiftkey.dev/rpm/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://rpm.packages.shiftkey.dev/gpg.key" > /etc/yum.repos.d/shiftkey-packages.repo'
+            sudo dnf update
+            sudo dnf install -y github-desktop
             notify-send "Software has been installed" --expire-time=10
            ;;
         6)  echo "Installing Oh-My-Zsh with Starship"
@@ -80,20 +85,11 @@ while [ "$CHOICE -ne 4" ]; do
             echo "eval "$(starship init zsh)"" >> ~/.zshrc
             notify-send "Starship Prompt Activated" --expire-time=10
            ;;
-        7)  echo "Installing Extras"
-            sudo dnf groupupdate -y sound-and-video
-            sudo dnf install -y libdvdcss
-            sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,ugly-\*,base} gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel ffmpeg gstreamer-ffmpeg
-            sudo dnf install -y lame\* --exclude=lame-devel
-            sudo dnf group upgrade -y --with-optional Multimedia
-	        sudo dnf copr enable peterwu/iosevka -y
-            sudo -s dnf -y copr enable dawid/better_fonts
-            sudo dnf update -y
-            sudo -s dnf install -y fontconfig-font-replacements
-            sudo -s dnf install -y fontconfig-enhanced-defaults
-	        sudo dnf update -y
-	        sudo dnf install -y iosevka-term-fonts jetbrains-mono-fonts-all gnome-shell-theme-flat-remix flat-remix-icon-theme flat-remix-theme terminus-fonts terminus-fonts-console google-noto-fonts-common mscore-fonts-all fira-code-fonts
-            source gsettings.sh
+        7)  echo "Installing Github Desktop"
+            sudo rpm --import https://rpm.packages.shiftkey.dev/gpg.key
+            sudo sh -c 'echo -e "[shiftkey-packages]\nname=GitHub Desktop\nbaseurl=https://rpm.packages.shiftkey.dev/rpm/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://rpm.packages.shiftkey.dev/gpg.key" > /etc/yum.repos.d/shiftkey-packages.repo'
+            sudo dnf update
+            sudo dnf install -y github-desktop
             notify-send "All done" --expire-time=10
            ;;
         8)  echo "Installing Nvidia Driver Akmod-Nvidia"
@@ -122,7 +118,6 @@ while [ "$CHOICE -ne 4" ]; do
             sudo dnf groupupdate -y core
             sudo dnf install -y rpmfusion-free-release-tainted
             sudo dnf install -y dnf-plugins-core
-            notify-send "RPM Fusion Enabled" --expire-time=10
             echo "Updating System Firmware"
             sudo fwupdmgr get-devices 
             sudo fwupdmgr refresh --force 
@@ -130,24 +125,23 @@ while [ "$CHOICE -ne 4" ]; do
             sudo fwupdmgr update
             echo "Speeding Up DNF"
             echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
-            notify-send "Your DNF config has now been amended" --expire-time=10
             echo "Enabling Flatpak"
             flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
             flatpak update
             source flatpak-install.sh
-            notify-send "Flatpak has now been enabled" --expire-time=10
             echo "Installing Software"
             sudo dnf install -y $(cat dnf-packages.txt)
-            notify-send "Software has been installed" --expire-time=10
+            sudo rpm --import https://rpm.packages.shiftkey.dev/gpg.key
+            sudo sh -c 'echo -e "[shiftkey-packages]\nname=GitHub Desktop\nbaseurl=https://rpm.packages.shiftkey.dev/rpm/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://rpm.packages.shiftkey.dev/gpg.key" > /etc/yum.repos.d/shiftkey-packages.repo'
+            sudo dnf update
+            sudo dnf install -y github-desktop
             echo "Installing Oh-My-Zsh with Starship"
             sudo dnf -y install zsh util-linux-user
             sh -c "$(curl -fsSL $OH_MY_ZSH_URL)"
             echo "change shell to ZSH"
             chsh -s "$(which zsh)"
-            notify-send "Oh-My-Zsh is ready to rock n roll" --expire-time=10
             curl -sS https://starship.rs/install.sh | sh
             echo "eval "$(starship init zsh)"" >> ~/.zshrc
-            notify-send "Starship Prompt Activated" --expire-time=10
             echo "Installing Tailscale"
             sudo dnf config-manager --add-repo -y https://pkgs.tailscale.com/stable/fedora/tailscale.repo
             sudo dnf install -y tailscale
